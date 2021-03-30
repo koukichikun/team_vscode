6ee3c6171eb84c372d6e2d0c15c46a87c50221ce
fukuyama manaka <m.fukuyama.sys20@morijyobi.ac.jp>
2021-03-30 12:34:21 +0900

    team_vscodeの追記
    en.htmlの作成
    kekka.htmlの作成

--- app.py
+++ app.py
@@ -1,10 +1,23 @@
-from flask import Flask,render_template
+from flask import Flask,render_template,request,redirect,session,url_for
+import string
+import os
 
 app=Flask(__name__)
 
 @app.route("/")
 def top():
     return render_template("index.html")
+
+@app.route("/en")
+def en():
+    return render_template("en.html")
+
+@app.route("/kekka",methods=['POST'])
+def kekka():
+    hankei=request.form.get("hankei")
+    ensyu=hankei*2*3.14
+    menseki=hankei*hankei*3.14
+    return render_template("kekka.html",menseki=menseki,ensyu=ensyu)
 
 if __name__=="__main__":
     app.run(debug=True)
